3f49f9997711a648559446583f71159f05f03bda
张凡 <zhang_fan@topsec.com.cn>
2022-04-27 12:02:45 +0800

    修复施肥时卡住问题

--- core/MainExecutor.js
+++ core/MainExecutor.js
@@ -423,7 +423,7 @@ function MainExecutor() {
                 if (text("点击领取").exists()) {
                     sleep(600)
                     common.clickByText("点击领取", 1000)
-                    sleep(600)
+                    sleep(1200)
                     common.clickByText("点击领取", 1000)
                     sleep(600)
                     common.clickByTextMatches(/收下去施肥|立即领取/, 1000)
